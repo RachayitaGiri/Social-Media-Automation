2e68e4b8d88756f05c2eef1d95e59c58fe72b55b
Rachayita Giri <rachayitagiri@gmail.com>
2017-08-29 14:59:56 +0530

    Introduced comments for understanding

--- dtree-csv.py
+++ dtree-csv.py
@@ -1,76 +1,38 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
-#from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
-
-#test=pd.read_csv('hindi.csv',header=None, sep=',', 
- #                names=['tweets', 'class'])   # columns names if no header
 try:
+	#The classifier, here, attempts to read the 'tweet-class.csv' file
+	#`header=None` means that the column names have not been given
+	#`sep=','` means that the columns of the csv are separated by commas
+	#`names=['tweets','class']` means that we have assigned names to the columns such that the first column represents
+	# the tweets while the second column contains the corresponding class labels 
 	df = pd.read_csv('tweet-class.csv', header=None, sep=',',names=['tweets', 'class'])   # columns names if no header
+	
+	#The TfidfVectorizer function transforms the input text into a feature vector
+	#fit_transform() is used to learn the vocabulary dictionary and return term-document matrix
 	vect = TfidfVectorizer()
 	x = vect.fit_transform(df['tweets'].values.astype('U')) 
 	y = df['class']
+	
+	#Setting parameters for the classifier.
 	SIZE=50
 	STEP=.02
-
+	#Dividing the tweet-class.csv file into training and test dataset in the ratio of 85%:15%
 	train,test,train_lab,test_lab=train_test_split(x,y,test_size=.15,random_state=100)
 	clf=DecisionTreeClassifier()
 	clf.fit(train,train_lab)
+	#Running the classifier and printing the output labels for the rows in the test dataset, along with the
+	#accuracy score
 	output=clf.predict(test)
 	print output
 	print accuracy_score(test_lab,output)
 
 except UnicodeDecodeError as e:
 	print 'unicode error'
-#a=load_iris()
-#a = open("iris.txt")
-#f.readline()  # skip the header
-#data = np.loadtxt(a)
-#x=a['data']
-#y=a['target']
-
-
-#def plot(train,train_lab):
- #   for index,row in enumerate(train):
-#        if train_lab[index]==0:
- #           c='r'
-#	    marker='>'
-#    	    q=plt.scatter(train[index,0],train[index,1],c=c,marker=marker,s=SIZE)
-#        elif train_lab[index]==1:
-#            c='b'
-#	    marker='s'
- #   	    w=plt.scatter(train[index,0],train[index,1],c=c,marker=marker,s=SIZE)
- #       else:
-#            c='g'
-#	    marker='+'
-#    	    e=plt.scatter(train[index,0],train[index,1],c=c,marker=marker,s=SIZE)
-#    plt.legend((q,w,e),('setosa','virginica','versicolor'),scatterpoints=1)
-
-#Uncomment to see the plot
-#plot(train,train_lab)
-#plt.show()
-
-#pca=PCA(n_components=2,whiten=True)
-#new_train=pca.fit_transform(train)
-#mean=new_train.mean(axis=0)
-#std=new_train.std(axis=0)
-#new_train=(new_train-mean)/std
-#clf.fit(new_train,train_lab)
-#Uncomment to see the plot
-#plot(new_train,train_lab)
-#x_min,x_max=new_train[:,0].min()-1,new_train[:,0].max()+1
-#y_min,y_max=new_train[:,1].min()-1,new_train[:,1].max()+1
-
-#xx,yy=np.meshgrid(np.arange(x_min,x_max,STEP),np.arange(y_min,y_max,STEP))
-#Z=clf.predict(np.c_[xx.ravel(),yy.ravel()])
-#Z=Z.reshape(xx.shape)
-#cs=plt.contourf(xx,yy,Z,cmap=plt.cm.Paired)
-#plot(new_train,train_lab)
-#plt.show()
